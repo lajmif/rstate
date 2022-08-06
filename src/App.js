218299a8cee59556d96f923be5cc4a60ee3b7df6
lajmif <sy.lajmi@gmail.com>
2022-08-06 15:25:16 +0100

    firstone

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+ state = {
+  todo: [
+    {
+       id: Math.random(),
+       task: "learn state",
+       isDone: false
+    },
+    {
+      id: Math.random(),
+      task: "wow",
+      isDone: false
+   },
+   {
+    id: Math.random(),
+    task: "its amazing",
+    isDone: false
+ },
+]
+    };
+
+
+ handleDelete = (id) => {
+  this.setState({todo: this.state.todo.filter((el) => el.id !=id) });
+ }
+
+
+
+    render () {
+      console.log(this.state.todo);
+
+      return (
+        <div style={{ textAlign: "center"}}>
+        {this.state.todo.map((el)  => (
+          <div style={{marginTop: "35px"}}>
+          <h1>{el.id}</h1>
+          <h3>{el.task}</h3>
+          <button>isDone</button>
+          <button onClick={() => this.handleDelete(el.id)}>delete</button>
+          </div>
+        ))}
+        </div>
+      );
+ 
+  }
 }
 
 export default App;
